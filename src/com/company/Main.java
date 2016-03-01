bf4b2005321abfc9c6d175f5f82adfb1d40574d4
Andrey Tatarenko <andrey17076@gmail.com>
2016-03-01 21:23:14 +0300

    Lots of fixes

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,64 +9,73 @@ public class Main {
     private static final int BROADCASTING_OPT = 1;
     private static final int EXIT_OPT = 2;
     private static final int IO_ERROR = 3;
+    private static final Scanner scanIn = new Scanner(System.in);
 
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
+        UDPClient client = new UDPClient();
+        client.start();
+
         int userOption;
-        Scanner scanIn = new Scanner(System.in);
-
+        System.out.printf("Usage: 0 - request for self; 1 - broadcasting; 2 - exit\n");
         do {
-            printUsage();
-
             try {
                 userOption = Integer.parseInt(scanIn.nextLine());
+                if (userOption == SELF_OPT || userOption == BROADCASTING_OPT)
+                    client.sendPackage(userOption);
             } catch (NumberFormatException e) {
                 userOption = IO_ERROR;
-            }
-
-            if (userOption == SELF_OPT || userOption == BROADCASTING_OPT) {
-                byte[] buf = new byte[256];
-
-                DatagramPacket packet = (userOption == SELF_OPT) ? getTime(buf) : broadcastTime(buf);
-                String receivedTime = new String(packet.getData());
-                System.out.println("Received time: "+receivedTime+"\n");
-
-            } else if (userOption >= IO_ERROR) {
-                System.out.println("Incorrect option! Try again!\n");
+                System.err.println(e);
             }
         } while (userOption != EXIT_OPT);
     }
-
-    public static void printUsage() {
-        System.out.printf("0 - require time for self\n1 - require time for all\n2 - exit\n");
-    }
-
-    private static DatagramPacket getTime(byte[] buffer) throws IOException {
-        DatagramSocket socket = new DatagramSocket();
-        InetAddress address = InetAddress.getByName("192.168.100.1");
-        DatagramPacket packet = new DatagramPacket(buffer, buffer.length, address, 8888);
-        socket.send(packet);
-
-        packet = new DatagramPacket(buffer, buffer.length);
-        socket.receive(packet);
-        socket.close();
-
-        return  packet;
-    }
-
-    private static DatagramPacket broadcastTime(byte[] buffer) throws IOException {
-        MulticastSocket socket = new MulticastSocket(8888);
-        InetAddress group = InetAddress.getByName("192.168.100.255");
-        socket.joinGroup(group);
-
-        DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
-        socket.receive(packet);
-        socket.leaveGroup(group);
-        socket.close();
-
-        return packet;
-    }
 }
 
+class UDPClient extends Thread {
+    protected DatagramSocket socket;
+    protected byte[] buffer;
+    protected InetAddress address;
+    private static final int PORT = 5555;
 
+    public UDPClient() {
+        super("UDP CLIENT THREAD");
+        buffer = new byte[256];
+
+        try {
+            address = InetAddress.getByName("10.211.55.2");
+        } catch (UnknownHostException e) {
+            System.err.println(e);
+        }
+
+        try {
+            socket = new DatagramSocket(PORT, address);
+            //socket.setBroadcast(true);
+        } catch (SocketException e) {
+            System.err.println(e);
+        }
+    }
+
+    public void sendPackage(int option) {
+        buffer[0] = (byte) option;
+        DatagramPacket packet = new DatagramPacket(buffer, buffer.length, address, PORT);
+        try {
+            socket.send(packet);
+        } catch (IOException e) {
+            System.err.println(e);
+        }
+    }
+
+    public void run() {
+        DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+        while (true) {
+            try {
+                socket.receive(packet);
+                String receivedTime = new String(packet.getData());
+                System.out.println("Received time: " + receivedTime + "\n");
+            } catch (IOException e) {
+                System.err.println(e);
+            }
+        }
+    }
+}
 
 
